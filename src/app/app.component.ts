a334c74f4c80ff3f292f43a0972fc4670a0fe3ca
lokals7 <42064801+lokals7@users.noreply.github.com>
2024-08-28 15:35:54 +0800

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'NG2024_Project';
+export class AppComponent { 
 }

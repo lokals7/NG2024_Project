7bc98209c1a891381cb61b8478fba0b04a2831a7
lokals7 <42064801+lokals7@users.noreply.github.com>
2024-09-11 23:03:22 +0800

    sept-11

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'Description', 'https://upload.wikimedia.org/wikipedia/commons/3/36/Pork_Hamonado_%28Mindanao%29_cooked_afritada-style_%28with_tomato_sauce%29.jpg'),
+  new Recipe('A Test Recipe', 'Description', 'https://upload.wikimedia.org/wikipedia/commons/3/36/Pork_Hamonado_%28Mindanao%29_cooked_afritada-style_%28with_tomato_sauce%29.jpg')
+];
 
+constructor(){
+
+}
+ngOnInit(){
+  
+}
 }

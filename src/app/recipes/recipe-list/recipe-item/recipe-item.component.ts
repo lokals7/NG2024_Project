74d1e96a7b6ba375cfcb544efa239148c5f67d74
lokals7 <42064801+lokals7@users.noreply.github.com>
2024-09-18 00:56:45 +0800

    9-18

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
+
+// @Input() recipeElement: {name: string, desc: string, imagePath: string};
+@Input() recipeElement: Recipe;
+@Input() recipeElIndex: number;
+@Output() recipeClicked = new EventEmitter<number>(); 
+recipeItemClick(recipeIndex: number) {
+  // throw new Error('Method not implemented.');
+  // console.log(recipeIndex);
+  this.recipeClicked.emit(recipeIndex);
+  }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,9 +6,11 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit{
-recipes: Recipe[] = [
-  new Recipe('A Test Recipe', 'Description', 'https://upload.wikimedia.org/wikipedia/commons/3/36/Pork_Hamonado_%28Mindanao%29_cooked_afritada-style_%28with_tomato_sauce%29.jpg'),
-  new Recipe('A Test Recipe', 'Description', 'https://upload.wikimedia.org/wikipedia/commons/3/36/Pork_Hamonado_%28Mindanao%29_cooked_afritada-style_%28with_tomato_sauce%29.jpg')
+  @Output() recipeSelectedClick = new EventEmitter<Recipe>();
+  recipeSelected: number; 
+  recipes: Recipe[] = [
+  new Recipe(`A Test Recipe 1`, 'Description 1', 'https://upload.wikimedia.org/wikipedia/commons/3/36/Pork_Hamonado_%28Mindanao%29_cooked_afritada-style_%28with_tomato_sauce%29.jpg'),
+  new Recipe('A Test Recipe 2', 'Description 2', 'https://www.maggi.ph/sites/default/files/styles/home_stage_1500_700/public/srh_recipes/6be23d6515053571fdebf14d9ea37e35.jpg?h=5c87dc81&itok=OS3FkDrS')
 ];
 
 constructor(){
@@ -16,5 +18,9 @@ constructor(){
 }
 ngOnInit(){
   
+}
+recipeEmitted(recipeIndex: number){
+this.recipeSelected = recipeIndex;
+this.recipeSelectedClick.emit(this.recipes[this.recipeSelected]);
 }
 }
